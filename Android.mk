3ddca592f70506fcb63e61bcd5085feb052ff81d
Nejcraft <nejcraftplays@gmail.com>
2019-09-11 15:01:02 +0200

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,10 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
-
-# include the non-open-source counterpart to this file
-
-
-#-include vendor/amlogic/mk8080bplus/AndroidBoardVendor.mk
+ifeq ($(TARGET_DEVICE),mk8080bplus)
+  include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
